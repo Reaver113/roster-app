11a43071d62c33a6aaaee07053e8db97a240f94c
Reaver113 <jasonalcock113@gmail.com>
2023-01-31 10:48:56 +1000

    calender added to change availabilites

--- src/ components/ChangeAvaliable.js
+++ src/ components/ChangeAvaliable.js
@@ -1,10 +1,39 @@
-function ChangeAvailable() {
-	return (
-		<div>
-		---insert calander---
-		</div>
-		)
-}
+import * as React from 'react';
+import dayjs from 'dayjs';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import "./ChangeAvailable.css"
 
+function ChangeAvailable() {
+	const date = new Date();
+
+	let day = date.getDate();
+	let month = date.getMonth() + 1;
+	let year = date.getFullYear();
+
+	let currentDate = `${day}-${month}-${year}`;
+
+  const [value, setValue] = React.useState(dayjs(currentDate));
+
+  return (
+		<>
+		<div className="Calendar">
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <StaticDatePicker
+        displayStaticWrapperAs="desktop"
+        openTo="day"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
+		</div>
+	</>
+  );
+}
 
 export default ChangeAvailable
