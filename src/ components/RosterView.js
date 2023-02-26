4bb4bc9e449c2acbcc9a2be3702d21c3c59f4a71
Reaver113 <jasonalcock113@gmail.com>
2023-02-27 01:30:15 +1000

    shifts added to roster page

--- src/ components/RosterView.js
+++ src/ components/RosterView.js
@@ -10,7 +10,7 @@ function RosterView({viewingRoster}) {
         {viewingRoster.shifts.map((mappedShifts) => (
           <div key={mappedShifts._id} className="rosterLine"><div className="namePlate">{mappedShifts.employee}</div>
             {hourIndex.map((hourBlock) => (
-              <div className="hourBlock" key={hourBlock}><p className="hour">{hourBlock}</p></div>
+              <div className={hoursToArray(mappedShifts.start, mappedShifts.end).includes(hourBlock) ? "rosteredBlock" : "hourBlock"} key={hourBlock}><p className="hour">{hourBlock}</p></div>
             ))}
           </div>
         ))}
--- src/utils.js
+++ src/utils.js
@@ -1,34 +1,33 @@
 
 export const dateConverter = (dateStr) => {
   const [yyyy, mm, dd, hh, mi] = dateStr.split(/[/:\-T]/);
-
   return `${dd}-${mm}-${yyyy}`;
-};
+}
 
 export const todaysDate = () => {
-	const date = new Date();
-	let day = date.getDate();
-	let month = date.getMonth() + 1;
-	let year = date.getFullYear();
-	let currentDate = `${day}-${month}-${year}`;
+	const date = new Date()
+	let day = date.getDate()
+	let month = date.getMonth() + 1
+	let year = date.getFullYear()
+	let currentDate = `${day}-${month}-${year}`
 	return currentDate
-};
+}
 
 export const getDayOfWeek = (dateString) => {
-  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const date = new Date(dateString);
-  const dayOfWeek = daysOfWeek[date.getUTCDay()];
-  return dayOfWeek;
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  const date = new Date(dateString)
+  const dayOfWeek = daysOfWeek[date.getUTCDay()]
+  return dayOfWeek
 }
 
 export const hoursToArray = (start, end) => {
-  const startDateTime = new Date(start);
-  const endDateTime = new Date(end);
-  const hours = [];
-  let currentDateTime = new Date(startDateTime);
+  const startDateTime = new Date(start)
+  const endDateTime = new Date(end)
+  const hours = []
+  let currentDateTime = new Date(startDateTime)
   while (currentDateTime <= endDateTime) {
-    hours.push(currentDateTime.getUTCHours());
-    currentDateTime.setTime(currentDateTime.getTime() + (60 * 60 * 1000));
+    hours.push(currentDateTime.getUTCHours())
+    currentDateTime.setTime(currentDateTime.getTime() + (60 * 60 * 1000))
   }
-  return hours;
+  return hours
 }
