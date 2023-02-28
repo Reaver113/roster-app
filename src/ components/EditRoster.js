7b4574ce99808a2c37e4b2444403790f9a82796b
Reaver113 <jasonalcock113@gmail.com>
2023-02-28 18:31:38 +1000

    box on click working, but not 100%

--- src/ components/EditRoster.js
+++ src/ components/EditRoster.js
@@ -1,17 +1,35 @@
 import { hoursToArray, colorHours } from "../utils.js"
+import { useState } from "react"
 import "./EditRoster.css"
 
 function EditRoster({viewingRoster, users}) {
 
-	let hourIndex = hoursToArray(viewingRoster.start, viewingRoster.end)
+  let hourIndex = hoursToArray(viewingRoster.start, viewingRoster.end)
+
+  const [blockClasses, setBlockClasses] = useState("hourBlock")
+
+  function checkRosteredHour(userId, hour) {
+		console.log(userId, hour)
+    setBlockClasses(prevBlockClasses => ({
+      ...prevBlockClasses,
+      [`${userId}-${hour}`]: prevBlockClasses[`${userId}-${hour}`] === "hourBlock" ? "rosteredHour" : "hourBlock"
+    }))
+  }
 
   return (
     <>
-		<div className="nameContainer">
-        {users.map((mappedShifts) => (
-          <div key={mappedShifts._id} className="rosterLine"><div className="namePlate">{mappedShifts.firstName}<br/> {mappedShifts.lastName}</div>
+      <div className="nameContainer">
+        {users.map((mappedUsers) => (
+          <div key={mappedUsers._id} className="rosterLine">
+            <div className="namePlate">{mappedUsers.firstName}<br/> {mappedUsers.lastName}</div>
             {hourIndex.map((hourBlock) => (
-              <div className={hoursToArray(mappedShifts.start, mappedShifts.end).includes(hourBlock) ? colorHours(hoursToArray(mappedShifts.start, mappedShifts.end)[0]) : "hourBlock"} key={hourBlock}><p className="hour">{hourBlock}</p></div>
+              <div
+                className={blockClasses[`${mappedUsers._id}-${hourBlock}`] || "hourBlock"}
+                onClick={() => checkRosteredHour(mappedUsers._id, hourBlock)}
+                key={hourBlock}
+              >
+                <p className="hour">{hourBlock}</p>
+              </div>
             ))}
           </div>
         ))}
